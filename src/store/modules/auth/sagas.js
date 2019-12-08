8aa4502106818f1adfebe60a96303e465e802b0b
Henrique Alexandre Torres <torresrick.dev@gmail.com>
2019-12-08 12:19:21 -0400

    Add loading info on sign in

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,27 +1,31 @@
 import { all, takeLatest, call, put } from 'redux-saga/effects';
 import history from '~/services/history';
 import api from '~/services/api';
-import actions, { signInSuccess } from './actions';
+import actions, { signInSuccess, signFailure } from './actions';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
-  console.tron.log(email, password);
-  const response = yield call(api.post, 'sessions', {
-    email,
-    password,
-  });
-  console.tron.log('2');
+  try {
+    const { email, password } = payload;
+    console.tron.log(email, password);
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
+    console.tron.log('2');
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  if (!user.provider) {
-    console.tron.error('Usuario nao e prestador de servico');
-    return;
+    if (!user.provider) {
+      console.tron.error('Usuario nao e prestador de servico');
+      return;
+    }
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (err) {
+    yield put(signFailure());
   }
-
-  yield put(signInSuccess(token, user));
-
-  history.push('/dashboard');
 }
 
 export default all([takeLatest(actions.SIGN_IN_REQUEST, signIn)]);
