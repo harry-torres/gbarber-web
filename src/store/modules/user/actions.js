52957afe271cbfb33a5c91bdedb173f963df1381
Henrique Alexandre Torres <torresrick.dev@gmail.com>
2019-12-08 11:44:26 -0400

    Add get user profile during sign in

---
+++ src/store/modules/user/actions.js
@@ -0,0 +1,7 @@
+const actions = {
+  SIGN_IN_REQUEST: '@auth/SIGN_IN_REQUEST',
+  SIGN_IN_SUCCESS: '@auth/SIGN_IN_SUCCESS',
+  SIGN_FAILURE: '@auth/SIGN_FAILURE',
+};
+
+export default actions;
--- src/store/modules/user/reducer.js
+++ src/store/modules/user/reducer.js
@@ -2,17 +2,14 @@ import produce from 'immer';
 import actions from './actions';
 
 const INITIAL_STATE = {
-  token: null,
-  signed: false,
-  loading: false,
+  profile: null,
 };
 
-export default function auth(state = INITIAL_STATE, action) {
+export default function user(state = INITIAL_STATE, action) {
   switch (action.type) {
     case actions.SIGN_IN_SUCCESS:
       return produce(state, draft => {
-        draft.token = action.payload.token;
-        draft.signed = true;
+        draft.profile = action.payload.user;
       });
     default:
       return state;
