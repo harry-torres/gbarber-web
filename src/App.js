0135fccc253b496b87eab1f272dbd2902015361f
Henrique Alexandre Torres <torresrick.dev@gmail.com>
2019-12-08 11:58:04 -0400

    Add persist authentication and user profile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
 import GlobalStyle from './styles/global';
-import store from './store';
+import { store, persistor } from './store';
 
 import Routes from './routes';
 import history from './services/history';
@@ -12,10 +13,12 @@ import history from './services/history';
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
